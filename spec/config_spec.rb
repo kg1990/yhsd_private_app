81dd160ac943efedcecaed7ed0729fb4db5ca44a
root <kg19902933@gmail.com>
2015-05-15 11:05:45 +0800

    add rspec test

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -42,6 +42,51 @@ module YhsdPrivateApp
       end
     end
 
+    context "init blank key" do
+      it "should raise error" do
+        expect{
+          YhsdPrivateApp::Config.initialize_connect!(
+            {
+              :app_key => '5c95892f62ce423a90726560f2de14f2', 
+              :app_secret => '', 
+              :auth_url => 'http://apps.localtest.com/oauth2/token', 
+              :api_host => "http://api.public.com/v1/"
+            }
+          )
+          }.to raise_error(MissingConfigKey)
+      end
+    end
+
+    context "init blank key" do
+      it "should raise error" do
+        expect{
+          YhsdPrivateApp::Config.initialize_connect!(
+            {
+              :app_key => '5c95892f62ce423a90726560f2de14f2', 
+              :app_secret => '33e910c2dbcd4f44881ad1d2c53ec2f8', 
+              :auth_url => '', 
+              :api_host => "http://api.public.com/v1/"
+            }
+          )
+          }.to raise_error(MissingConfigKey)
+      end
+    end
+
+    context "init blank key" do
+      it "should raise error" do
+        expect{
+          YhsdPrivateApp::Config.initialize_connect!(
+            {
+              :app_key => '5c95892f62ce423a90726560f2de14f2', 
+              :app_secret => '33e910c2dbcd4f44881ad1d2c53ec2f8', 
+              :auth_url => 'http://apps.localtest.com/oauth2/token', 
+              :api_host => ""
+            }
+          )
+          }.to raise_error(MissingConfigKey)
+      end
+    end
+
     context "init blank yml" do
       it "should raise error" do
         expect{
